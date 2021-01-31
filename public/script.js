74938690a09fdabe0e24f975caf94695d8f58982
BriceAbe <briceabe@gmail.com>
2021-01-31 17:58:40 +0100

    connection autre utilisteurs au flux video

--- public/script.js
+++ public/script.js
@@ -1,3 +1,10 @@
+const socket = io("/");
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "443",
+});
 // creer un element video
 
 const videoID = document.getElementById("video");
@@ -15,18 +22,37 @@ navigator.mediaDevices
   .then((stream) => {
     // recuperer le flux video
     videoStream = stream;
-
     addVideoStream(videoElement, stream);
+    //
+    socket.on("user-connected", (userId) => {
+      userConnected(userId, stream);
+    });
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      socket.emit("join-room", RoomID, id);
+    });
   });
+
+peer.on("open", (id) => {
+  socket.emit("join-room", RoomID, id);
+});
+
+//  noouvelle personne qui rentre sans le steram
+const userConnected = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const videoElement = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    // addVideoStream(videoElement, userVideoStream);
+  });
+};
 
 //fonction joue la video du stream
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
-
   video.addEventListener("loadedmetadata", () => {
     video.play();
   });
-  console.log(video);
   videoID.append(video);
 };
--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require("express");
-
+require("dotenv").config();
 const app = express();
 
 // creation d un server http
 const server = require("http").Server(app);
 
+// import socket io pour la communication en temps reel entre serveur et client
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
 // generer des id unique
 const { v4: uuid } = require("uuid");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
-const PORT = process.env.PORT || 3045;
+const PORT = process.env.PORT;
 
 // utilisation de  moteur de fichier ejs
 app.set("view engine", "ejs");
 
+app.use("/peerjs", peerServer);
 // utilisation de fichier static
 app.use(express.static("public"));
 
@@ -23,7 +30,14 @@ app.get("/:room", (req, res) => {
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuid()}`);
-  // res.status(200).render("room");
+  // res.status(200).render("flux");
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
 });
 
 server.listen(PORT, () => {
